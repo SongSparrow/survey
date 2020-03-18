6cbbf6a38414250ab80bd678b5586a8a060723e8
120512900 <51316715+120512900@users.noreply.github.com>
2020-03-18 11:39:57 +0800

    1

--- app/src/main/java/com/example/survey/Response.java
+++
@@ -1,9 +0,0 @@
-package com.example.survey;
-
-import java.io.Serializable;
-
-public interface Response extends Serializable {
-    public Boolean isResponse();
-    public void setResponse(String response);
-    public String getResponse();
-}
